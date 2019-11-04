aff4117d317267a9739419e296ed83fc848a07b0
priyankasam86 <priyanka_dalvi86@yahoo.co.in>
2019-11-04 14:21:49 +0530

    Commit

---
+++ main1.js
@@ -0,0 +1,35 @@
+let strMonth="Mon"
+
+switch(strMonth){
+    case "Mon":
+        console.log("1st Day of Week.");
+        break;
+
+    case "Tue":
+        console.log("2nd Day of Week.");
+        break;
+
+    case "Wed":
+        console.log("3rd Day of Week.");
+        break;
+
+    case "Thu":
+        console.log("4th Day of Week.");
+        break;
+
+    case "Fri":
+        console.log("5th Day of Week.");
+        break;
+
+    case "Sat":
+        console.log("6th Day of Week.");
+        break;
+    
+    case "Sun":
+        console.log("7th Day of Week.");
+        break;
+
+    default:
+        console.log("Please enter Proper Day of Week.");
+
+}
